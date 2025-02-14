fb06149b47fb5245d4ae6aca67350dfd3aca036f
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-14 14:48:20 +0600

    Написал метод для запуска EchoServer

--- EchoServer41/src/Main.java
+++ EchoServer41/src/Main.java
@@ -1,5 +1,7 @@
+import server.EchoServer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EchoServer.bindToPort(4040).run();
     }
 }
